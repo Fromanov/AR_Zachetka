1f868ae92fa9b9e60f0c9e938413a5829b156d20
Froman <agbuha@gmail.com>
2019-10-24 19:28:33 +1000

    Update
    Add another missing files

---
+++ Zachetka(Test)/Assets/Character Creator UI/Scripts/GetUMARootPart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Michsky.UI.CCUI
+{
+    public class GetUMARootPart : MonoBehaviour
+    {
+        [Header("SETTINGS")]
+        public string rootPartName;
+
+        GameObject tempObj;
+
+        void LateUpdate()
+        {
+            tempObj = GameObject.Find(rootPartName);
+            if(tempObj != null)
+            {
+                gameObject.transform.SetParent(tempObj.transform);
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ Zachetka(Test)/Assets/Character Creator UI/Scripts/UMADnaSlider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Michsky.UI.CCUI
+{
+    public class UMADnaSlider : MonoBehaviour
+    {
+        [Header("RESOURCES")]
+        public UMACustomizer customizerScript;
+        public Slider mainSlider;
+
+        [Header("SETTINGS")]
+        public string DNATag = "height";
+
+        IEnumerator GetSliderValue()
+        {
+            yield return new WaitForSeconds(0.1f);
+            mainSlider.value = customizerScript.dna[DNATag].Get();
+            StopCoroutine("GetSliderValue");
+        }
+
+        void Start()
+        {
+            StartCoroutine("GetSliderValue");
+        }
+
+        void OnEnable()
+        {
+            mainSlider.onValueChanged.AddListener(ChangeDNA);
+        }
+
+        void OnDisable()
+        {
+            mainSlider.onValueChanged.RemoveListener(ChangeDNA);
+        }
+
+        public void ChangeDNA(float value)
+        {
+            customizerScript.dna[DNATag].Set(value);
+            customizerScript.avatar.BuildCharacter();
+        }
+    }
+}
