aa77a6e9b73159b7e120a9d279f113588de5a6fc
Froman <agbuha@gmail.com>
2019-12-12 19:09:13 +1000

    Update 12.12.2019
    Fix terms of use and lab rules.

---
+++ Zachetka(Test)/Assets/PanelsHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsHandler : MonoBehaviour
+{
+	/*public GameObject rulesButton;
+	public GameObject termsButton;*/
+	public GameObject rulesPanel;
+	public GameObject singUpPanel;	
+	
+    
+	public void TermsOfUseButton()
+	{
+		Application.OpenURL("https://www.iubenda.com/privacy-policy/37730946/full-legal");
+	}
+
+	public void RulesOfLabButton()
+	{
+		singUpPanel.SetActive(false);
+		rulesPanel.SetActive(true);
+	}
+
+	public void BackButton()
+	{
+		singUpPanel.SetActive(true);
+		rulesPanel.SetActive(false);
+	}
+}
